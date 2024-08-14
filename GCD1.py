9ebd31edbc1703d8eb1d70b031e19eaaaa98518b
THANTANIN <148644308+THANTANIN@users.noreply.github.com>
2024-08-14 21:19:56 +0700

    Add files via upload
    GCD with chat GPT

--- GCD1.py
+++ GCD1.py
@@ -2,6 +2,9 @@
 import numpy as np
 import math
 count =0
+digit_record= []
+count_record= []
+testcase_file = "Regular_Case1.txt"
 def prime_factors(n):
     global count
     lst = []
@@ -25,36 +28,38 @@ def FindGCD1(*arguments):
     input = []
     array = []
     for i in arguments:
-     input.append(i)
+        input.append(int(i))
     global count
-
-
-    result =[]
 
     for i in range(len(input)):
         array.append(prime_factors(input[i]))
-    for i in range(len(array)):
-        current = []
-        min  =[]
-        max= []
-        add = []
-        if (i ==0 ): current = array[0]
-        else: current = result
-        if (len(current) >= len(array[i])):
-            max = current
-            min= array[i]
-        else:
-            max =array[i]
-            min = current
-        for i in min:
-            for j in max:
-                if (i ==j):
-                    add.append(i)
-                    break
-        result = add
+    result = array[0]
+    for i in range(1, len(array)):
+        current_factors = array[i]
+        common_factors = []
+        temp_result = result[:]
+
+        for factor in current_factors:
+            if factor in temp_result:
+                common_factors.append(factor)
+                temp_result.remove(factor)
+        result = common_factors
     ans = 1
     for i in result:
         ans *= i
-    print(count)
-    return ans
-print(FindGCD1( 36,84,120,77,59,19))
+    print("Count:", count)
+    print("GCD:", ans)
+with open(testcase_file, "r") as f:
+    for i in f:
+        num = []
+        digit_num = []
+        count= 0
+        str_arr =i.strip().split(",")
+        #print(str_arr)
+        for i in str_arr:
+            num.append(int(i))
+            digit_num.append(len(str(i)))
+        print(num)
+        digit_record.append(sum(digit_num)- len(digit_num))
+        FindGCD1(*num[:len(num)+1])
+
