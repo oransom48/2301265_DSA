c89101025e8bab98d0fc23464cc6953becf7c1aa
oransom48 <kornrawee.kochtat@gmail.com>
2024-08-19 09:40:23 +0700

    update graph

--- lab1_Algorithm analysis/gcd1.py
+++ lab1_Algorithm analysis/gcd1.py
@@ -1,10 +1,12 @@
 # importing the required module
 import matplotlib.pyplot as plt
 import math
+
 count =0
 digit_record= []
 count_record= []
 testcase_file = "lab1_Algorithm analysis/testcase/Extra Case2 plot.txt"
+
 def prime_factors(n):
     global count
     lst = []
@@ -15,24 +17,27 @@ def prime_factors(n):
         count += 1
 
     for i in range(3, int(math.sqrt(n)) + 1, 2):
+        count += 1
         while n % i == 0:
             lst.append(i)
             n = n / i
-            count += 1
+            # count += 1
 
     if n > 2:
         lst.append(int(n))
     return lst
-def FindGCD1(*arguments):
 
+def FindGCD1(*arguments):
     input = []
     array = []
+    
     for i in arguments:
         input.append(int(i))
     global count
 
     for i in range(len(input)):
         array.append(prime_factors(input[i]))
+
     result = array[0]
     for i in range(1, len(array)):
         current_factors = array[i]
@@ -44,11 +49,14 @@ def FindGCD1(*arguments):
                 common_factors.append(factor)
                 temp_result.remove(factor)
         result = common_factors
+
     ans = 1
     for i in result:
         ans *= i
+
     print("Count:", count)
     print("GCD:", ans)
+
 with open(testcase_file, "r") as f:
     for i in f:
         num = []
@@ -63,8 +71,9 @@ with open(testcase_file, "r") as f:
         digit_record.append(sum(digit_num)- len(digit_num))
         FindGCD1(*num[:len(num)+1])
         count_record.append(count)
+
 plt.plot(digit_record,count_record)
 plt.xlabel('Digits')
 plt.ylabel('Count')
-plt.grid(True)
+# plt.grid(True)
 plt.savefig("lab1_Algorithm analysis/graph/gcd1_ExtraCase2.png")
