d66a13b45bf29d37fb25c4b3256e9bdd5e26747c
pinwap <156493711+pinwap@users.noreply.github.com>
2024-08-20 20:17:02 +0700

    input

--- lab2_stackqueue/Lab2_2/src/Main.java
+++ lab2_stackqueue/Lab2_2/src/Main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        String expression = "( ( 1 + 2 ) * 4 ) * 3";
+        String expression;
+        Scanner input = new Scanner(System.in);
+        expression = input.nextLine();
 
         System.out.println(ArithmeticEvaluation.evaluate(expression));
 
